575fe20539a41c0f5345990498bcf07bf180d1f7
engineerkks6 <engineerk.k.s6@gmail.com>
2021-03-27 15:58:43 +0530

    Tree is Built

--- ApnaCollege/OOPs/BinaryTree/bt.cpp
+++ ApnaCollege/OOPs/BinaryTree/bt.cpp
@@ -26,4 +26,28 @@ int main()
         / \
        2  3
     */
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    /* 
+         1  
+        / \
+       2  3
+      / \
+     4   5
+
+    */
+     root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    /* 
+
+           1  
+         /  \
+       2     3
+      / \   / \
+     4   5 6   7
+
+   */
 }
